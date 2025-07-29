633762a6467d57b9c4679c2cb7349b0b4d63a8ee
Charles <charlesfbonfim@gmail.com>
2025-07-29 12:36:18 +0000

    https://i.imgur.com/ZOPB1Hp.png essa sera logo do projeto

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,28 +1,19 @@
 import * as React from 'react';
+import Image from 'next/image';
 import { cn } from "@/lib/utils"
 
-export const Logo = ({ className, ...props }: React.SVGProps<SVGSVGElement>) => (
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className={cn("text-primary", className)}
+type LogoProps = {
+    className?: string;
+};
+
+export const Logo = ({ className, ...props }: LogoProps) => (
+    <Image
+        src="https://i.imgur.com/ZOPB1Hp.png"
+        alt="Comunidade Zero Lactose Logo"
+        width={512}
+        height={512}
+        className={cn(className)}
+        priority
         {...props}
-    >
-        <path d="M3 7l2 2.5v10l-2 2.5" />
-        <path d="M21 7l-2 2.5v10l2 2.5" />
-        <path d="M8 7h8" />
-        <path d="M8 12h8" />
-        <path d="M8 17h8" />
-        <path d="M5 9.5L8 7" />
-        <path d="M5 14.5l3-2.5" />
-        <path d="M5 19.5l3-2.5" />
-        <path d="M19 9.5L16 7" />
-        <path d="M19 14.5l-3-2.5" />
-        <path d="M19 19.5l-3-2.5" />
-    </svg>
+    />
 );
